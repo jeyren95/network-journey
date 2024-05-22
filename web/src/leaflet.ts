cee4ad50960950c28d595501865d2ce509a86a93
jeyren95 <jeyren95@hotmail.com>
2024-05-22 23:08:26 +0800

    Minor changes

--- web/src/leaflet.ts
+++ web/src/leaflet.ts
@@ -4,4 +4,3 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-console.log("hello")
