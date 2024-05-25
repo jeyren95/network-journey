1e52a1a78bf996a7c42cdd763f79a4c4b75276eb
jeyren95 <jeyren95@hotmail.com>
2024-05-26 02:17:46 +0800

    Update formatting

--- web/src/leaflet.ts
+++ web/src/leaflet.ts
@@ -1,8 +1,8 @@
 import * as L from "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet-src.esm.js";
 
-var map = L.map("map").setView([51.505, -0.09], 13)
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var map = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+	attribution:
+		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-
--- web/src/server.ts
+++ web/src/server.ts
@@ -1,8 +1,8 @@
 import {
-  createServer,
-  type ServerResponse,
-  type OutgoingHttpHeaders,
-  type IncomingMessage,
+	createServer,
+	type ServerResponse,
+	type OutgoingHttpHeaders,
+	type IncomingMessage,
 } from "http";
 import { readFileSync } from "fs";
 
@@ -12,7 +12,7 @@ const BUFFERS = {
 	notFoundPage: readFileSync("src/pages/404.html"),
 	stylesheet: readFileSync("src/index.css"),
 	leafletScript: readFileSync("dist/leaflet.js"),
-}
+};
 
 enum URL {
 	HOME_PAGE = "/",
@@ -27,36 +27,36 @@ enum MimeType {
 }
 
 const writeStream = (
-  res: ServerResponse<IncomingMessage>,
-  headers: OutgoingHttpHeaders,
-  statusCode: number,
-  buffer: Buffer,
+	res: ServerResponse<IncomingMessage>,
+	headers: OutgoingHttpHeaders,
+	statusCode: number,
+	buffer: Buffer,
 ) => {
-  res.writeHead(statusCode, headers);
-  res.write(buffer);
-  res.end();
+	res.writeHead(statusCode, headers);
+	res.write(buffer);
+	res.end();
 };
 
 const server = createServer((req, res) => {
-  const url = req.url;
-  let headers: OutgoingHttpHeaders = {};
+	const url = req.url;
+	let headers: OutgoingHttpHeaders = {};
 
-  switch (url) {
-    case URL.LEAFLET_SCRIPT:
-      headers["Content-Type"] = MimeType.JS;
-      writeStream(res, headers, 200, BUFFERS.leafletScript);
-      break;
-    case URL.STYLESHEET:
-      headers["Content-Type"] = MimeType.CSS;
-      writeStream(res, headers, 200, BUFFERS.stylesheet);
-      break;
+	switch (url) {
+		case URL.LEAFLET_SCRIPT:
+			headers["Content-Type"] = MimeType.JS;
+			writeStream(res, headers, 200, BUFFERS.leafletScript);
+			break;
+		case URL.STYLESHEET:
+			headers["Content-Type"] = MimeType.CSS;
+			writeStream(res, headers, 200, BUFFERS.stylesheet);
+			break;
 		case URL.HOME_PAGE:
-      headers["Content-Type"] = MimeType.HTML;
+			headers["Content-Type"] = MimeType.HTML;
 			writeStream(res, headers, 200, BUFFERS.homePage);
 			break;
-    default:
-      headers["Content-Type"] = MimeType.HTML;
-  		writeStream(res, headers, 404, BUFFERS.notFoundPage);
+		default:
+			headers["Content-Type"] = MimeType.HTML;
+			writeStream(res, headers, 404, BUFFERS.notFoundPage);
 	}
 });
 
@@ -64,5 +64,5 @@ const PORT = 3000;
 const HOST = "127.0.0.1";
 
 server.listen(PORT, HOST, () => {
-  console.log("Server listening on port 3000");
+	console.log("Server listening on port 3000");
 });
