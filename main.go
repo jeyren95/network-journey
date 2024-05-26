b67680b71ae805251cf5c4ff16d700e28df89e91
jeyren95 <jeyren95@hotmail.com>
2024-05-26 16:49:50 +0800

    Update formatting

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jeyren95/network-journey/controllers"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.POST("/ip", controllers.IpHops)
 
 	r.Run()
