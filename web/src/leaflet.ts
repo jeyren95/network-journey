39bc8bc27c9af21b72cb60dbfc391d14c3a7dae6
jeyren95 <jeyren95@hotmail.com>
2024-06-04 11:26:07 +0800

    Added styles

--- web/src/leaflet.ts
+++ web/src/leaflet.ts
@@ -1,12 +1,12 @@
 import {
-  LatLng,
-  Map,
-  Marker,
-  TileLayer,
-  Polyline,
-  type PolylineOptions,
-  type TileLayerOptions,
-  type LatLngExpression,
+	LatLng,
+	Map,
+	Marker,
+	TileLayer,
+	Polyline,
+	type PolylineOptions,
+	type TileLayerOptions,
+	type LatLngExpression,
 } from "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet-src.esm.js";
 import type { GetIpHopsRes, IpHopsOptions } from "./types";
 import { getIpHops } from "./services";
@@ -15,13 +15,13 @@ import { getIpHops } from "./services";
 const TILE_LAYER_URL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 const DEFAULT_MAX_ZOOM = 19;
 const ATTRIBUTION =
-  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+	'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
 const map = new Map("map");
 const tileLayerOptions: TileLayerOptions = {
-  maxZoom: DEFAULT_MAX_ZOOM,
-  attribution: ATTRIBUTION,
-  noWrap: true,
+	maxZoom: DEFAULT_MAX_ZOOM,
+	attribution: ATTRIBUTION,
+	noWrap: true,
 };
 const tileLayer = new TileLayer(TILE_LAYER_URL, tileLayerOptions);
 tileLayer.addTo(map);
@@ -30,43 +30,51 @@ map.fitWorld();
 // add ui layer methods
 const MARKER_ZOOM_AROUND = 15;
 const addMarker = (latlng: LatLngExpression) => {
-  const marker = new Marker(latlng);
-  marker.on("click", () => {
-    map.setZoomAround(latlng, MARKER_ZOOM_AROUND);
-  });
-  marker.addTo(map);
+	const marker = new Marker(latlng);
+	marker.on("click", () => {
+		map.setZoomAround(latlng, MARKER_ZOOM_AROUND);
+	});
+	marker.addTo(map);
 };
 
 // add vector layer methods
 const VIEW_ZOOM = 10;
 const addPolyline = (latlngs: LatLngExpression[], options: PolylineOptions) => {
-  const polyline = new Polyline(latlngs, options);
-  polyline.addTo(map);
-  map.setView(latlngs[0], VIEW_ZOOM);
+	const polyline = new Polyline(latlngs, options);
+	polyline.addTo(map);
+	map.setView(latlngs[0], VIEW_ZOOM);
 };
 
 // drawing the map
-const form = document.querySelector("form");
-(form as HTMLFormElement).addEventListener("submit", (event) => {
-  event.preventDefault();
+const DEFAULT_POLYLINE_COLOR = "red";
+const form = <HTMLFormElement>document.querySelector("form");
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
 
-  const hostname = (<HTMLInputElement>document.querySelector("#hostname"))
-    .value;
-  const maxHops = (<HTMLInputElement>document.querySelector("#max-hops")).value;
-  const waitTime = (<HTMLInputElement>document.querySelector("#wait-time"))
-    .value;
-  const requestBody: IpHopsOptions = {
-    hostname,
-    waitTime: Number(waitTime),
-    maxHops: Number(maxHops),
-  };
+	// disable form
+	const fieldset = <HTMLFieldSetElement>document.querySelector("fieldset");
+	fieldset.disabled = true;
 
-  getIpHops(requestBody)
-    .then((res) => res.json())
-    .then((data: GetIpHopsRes) => {
-      const latlngs = data.geolocations.map((h) => new LatLng(h.lat, h.lon));
-      latlngs.forEach((l: LatLng) => addMarker(l));
-      addPolyline(latlngs, { color: "red" });
-    })
-    .catch((err) => console.log(err));
+	const hostname = (<HTMLInputElement>document.querySelector("#hostname"))
+		.value;
+	const maxHops = (<HTMLInputElement>document.querySelector("#max-hops")).value;
+	const waitTime = (<HTMLInputElement>document.querySelector("#wait-time"))
+		.value;
+	const requestBody: IpHopsOptions = {
+		hostname,
+		waitTime: Number(waitTime),
+		maxHops: Number(maxHops),
+	};
+
+	getIpHops(requestBody)
+		.then((res) => res.json())
+		.then((data: GetIpHopsRes) => {
+			const latlngs = data.geolocations.map((h) => new LatLng(h.lat, h.lon));
+			latlngs.forEach((l: LatLng) => addMarker(l));
+			addPolyline(latlngs, { color: DEFAULT_POLYLINE_COLOR });
+		})
+		.catch((err) => console.log(err))
+		.finally(() => {
+			fieldset.disabled = false;
+		});
 });
